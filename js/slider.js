7dc456c10b9599681906eefa09fd431320ee1435
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-01-05 13:14:22 +0100

    menu & slider functionality

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var nextBtn = document.querySelector('#nextPicture');
+    var prevBtn = document.querySelector('#prevPicture');
+    var imgList = document.querySelectorAll('.sliderImages li');
+    var index = 0;
+    var lastChange = 0;
+
+    function nextImg() {
+        imgList[index].classList.remove('visible');
+        (index === imgList.length -1) ? index = 0 : index++;
+        imgList[index].classList.add('visible');
+        lastChange = 0;
+    }
+
+    function prevImg() {
+        imgList[index].classList.remove('visible');
+        (index === 0) ? index = imgList.length -1 : index--;
+        imgList[index].classList.add('visible');
+        lastChange = 0;
+    }
+
+    imgList[index].classList.add('visible');
+
+    //SLIDER
+
+    nextBtn.addEventListener('click', nextImg);
+    prevBtn.addEventListener('click', prevImg);
+
+    setInterval(function () {
+        if (lastChange === 7 /*amount of seconds between slide*/){
+            return nextImg();
+        }
+        lastChange += 1;
+    }, 1000);
+});
